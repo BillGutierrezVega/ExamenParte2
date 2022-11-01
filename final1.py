c78d14311db4ced3cc1139af82dd86a133c8fbfe
BillGutierrezVega <darckneck@hotmail.com>
2022-11-01 09:41:08 -0500

    Tarea1: Agregando clase Libro y sus métodos

---
+++ final1.py
@@ -0,0 +1,39 @@
+
+class Libro:
+    def __init__(self,id, título, género, ISBN, editorial,autores):
+        self.id = id
+        self.título = título
+        self.género = género
+        self.ISBN = ISBN
+        self.editorial = editorial
+        self.autores = autores
+        
+    def leer_archivo(self):
+        pass
+    
+    def listar_libro(self):
+        pass
+    
+    def agregar_libro(self):
+        pass
+    
+    def eliminar_libro(self):
+        pass
+    
+    def buscar_libro(self):
+        pass
+    
+    def ordenar_titulo(self):
+        pass
+    
+    def buscar_libro2(self):
+        pass
+    
+    def buscar_libro3(self):
+        pass
+    
+    def editar_libro(self):
+        pass
+    
+    def guardar_libro(self):
+        pass
